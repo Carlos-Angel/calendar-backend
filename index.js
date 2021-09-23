18cca5accf6e5e2d1c24bd826543e3deef2aae16
Charly_Angel <c.a.a.a.503@gmail.com>
2021-09-22 21:21:13 -0500

    feat: archivo de rutas para autenticación

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const { port } = require('./config');
+const auth = require('./routes/auth');
 
 const app = express();
 
 app.use(express.static('public' ));
+
+/** routes */
+app.use('/api/auth', auth)
 
 app.listen(port, () => {
   console.log(`server listening on port ${port}`);
