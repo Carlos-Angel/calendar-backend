c65fb77426dc0e0c0848e5032992f8933f3c9686
Charly_Angel <c.a.a.a.503@gmail.com>
2021-09-22 21:12:56 -0500

    feat: carpeta public

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ const { port } = require('./config');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.json({ hello: 'calendar backend' });
-});
+app.use(express.static('public' ));
 
 app.listen(port, () => {
   console.log(`server listening on port ${port}`);
