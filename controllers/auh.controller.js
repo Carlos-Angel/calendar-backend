bbcf43bc87ec7ed66556a0933ba1bf1d59b77ba3
Charly_Angel <c.a.a.a.503@gmail.com>
2021-09-29 21:16:38 -0500

    feat: express validator

--- controllers/auh.controller.js
+++ controllers/auh.controller.js
@@ -1,19 +1,36 @@
 const { request, response } = require('express');
+const { validationResult } = require('express-validator');
 
 const signIn = (req = request, res = response, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      ok: false,
+      errors: errors.mapped(),
+    });
+  }
+
   res.json({ msg: ' sign in' });
 };
 
 const login = (req = request, res = response, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      ok: false,
+      errors: errors.mapped(),
+    });
+  }
+
   res.json({ msg: 'login' });
 };
 
 const resetToken = (req = request, res = response, next) => {
-  res.json({msg: 'reset token'})
-}
+  res.json({ msg: 'reset token' });
+};
 
 module.exports = {
   signIn,
   login,
-  resetToken
+  resetToken,
 };
--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,31 @@
-const {Router} = require('express');
+const { Router } = require('express');
+const { check } = require('express-validator');
 const authController = require('../controllers/auh.controller');
 const router = Router();
 
-
 /** rutas: /api/auth */
 
-router.post('/sign-in',authController.signIn);
-router.post('/login',authController.login);
-router.get('/reset-token',authController.resetToken);
+router.post(
+  '/sign-in',
+  [
+    check('name', 'name is required').not().isEmpty(),
+    check('email', 'email not valid').isEmail(),
+    check('password', 'password must be than 5 characters. ').isLength({
+      min: 6,
+    }),
+  ],
+  authController.signIn,
+);
+router.post(
+  '/login',
+  [
+    check('email', 'email not valid').isEmail(),
+    check('password', 'password must be than 5 characters. ').isLength({
+      min: 6,
+    }),
+  ],
+  authController.login,
+);
+router.get('/reset-token', authController.resetToken);
 
-
-module.exports = router
+module.exports = router;
