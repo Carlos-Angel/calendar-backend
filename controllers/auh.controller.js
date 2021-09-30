72feb7a001bf5561dd6d0d196a1aadba5ca6f847
Charly_Angel <c.a.a.a.503@gmail.com>
2021-09-29 22:38:39 -0500

    feat: acción para registar un usuario

--- controllers/auh.controller.js
+++ controllers/auh.controller.js
@@ -1,7 +1,22 @@
 const { request, response } = require('express');
+const debug = require('debug')('calendar:auth');
+const User = require('../models/User.model');
 
-const signIn = (req = request, res = response, next) => {
-  res.json({ msg: ' sign in' });
+const signIn = async (req = request, res = response, next) => {
+  const user = new User(req.body);
+  try {
+    await user.save();
+  } catch (error) {
+    debug(error.message);
+    return res.status(500).json({
+      ok: false,
+      msg: 'internal server error'
+    });
+  }
+  res.json({
+    ok: true,
+    msg: 'user created'
+  });
 };
 
 const login = (req = request, res = response, next) => {
