095ccccd6572fd49e61c23ccd6ef99ce831736c2
Charly_Angel <c.a.a.a.503@gmail.com>
2021-09-29 22:44:24 -0500

    feat: validar email único

--- controllers/auh.controller.js
+++ controllers/auh.controller.js
@@ -5,17 +5,27 @@ const User = require('../models/User.model');
 const signIn = async (req = request, res = response, next) => {
   const user = new User(req.body);
   try {
+    const existEmail = await User.findOne({email: user.email});
+
+    if(existEmail){
+      return res.status(400).json({
+        ok: false,
+        msg: 'email or password not valid',
+      });
+    }
+
+
     await user.save();
   } catch (error) {
     debug(error.message);
     return res.status(500).json({
       ok: false,
-      msg: 'internal server error'
+      msg: 'internal server error',
     });
   }
   res.json({
     ok: true,
-    msg: 'user created'
+    msg: 'user created',
   });
 };
 
