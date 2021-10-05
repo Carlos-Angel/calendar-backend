48f2ffa3b33dd0f8f85a1006731801dafbf3c427
Charly_Angel <c.a.a.a.503@gmail.com>
2021-10-05 12:33:01 -0500

    feat: validar campos al crear evento

--- routes/events.js
+++ routes/events.js
@@ -1,6 +1,9 @@
 const { Router } = require('express');
+const { check } = require('express-validator');
 const eventController = require('../controllers/event.controller');
+const { isDate } = require('../helpers/isDate');
 const router = Router();
+const { fieldValidator } = require('../middlewares/field-validator');
 const { jwtValidator } = require('../middlewares/jwt-validator');
 
 /** rutas: /api/event */
@@ -8,7 +11,16 @@ const { jwtValidator } = require('../middlewares/jwt-validator');
 router.use(jwtValidator);
 router.get('/', eventController.getEvents);
 router.get('/:id', eventController.getEvent);
-router.post('/', eventController.addEvent);
+router.post(
+  '/',
+  [
+    check('title', 'name is required').not().isEmpty(),
+    check('start', 'start date is required').custom(isDate),
+    check('end', 'end date is required').custom(isDate),
+    fieldValidator,
+  ],
+  eventController.addEvent,
+);
 router.put('/:id', eventController.updateEvent);
 router.delete('/:id', eventController.deleteEvent);
 
