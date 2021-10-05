ec286afdf860268c6d77fb882b08cb14558ea3cd
Charly_Angel <c.a.a.a.503@gmail.com>
2021-10-05 12:52:55 -0500

    feat: acción para guardar un evento

--- controllers/event.controller.js
+++ controllers/event.controller.js
@@ -1,5 +1,6 @@
 const { request, response } = require('express');
 const debug = require('debug')('calendar:event');
+const Event = require('../models/Event.model');
 
 const getEvents = (req = request, res = response, next) => {
   return res.json({ ok: true, msg: 'getEvents' });
@@ -9,8 +10,26 @@ const getEvent = (req = request, res = response, next) => {
   return res.json({ ok: true, msg: 'getEvent' });
 };
 
-const addEvent = (req = request, res = response, next) => {
-  return res.json({ ok: true, msg: 'addEvent' });
+const addEvent = async (req = request, res = response, next) => {
+  const event = req.body;
+  event.user = req.user.uid;
+  try {
+    const addEvent = new Event(event);
+    await addEvent.save();
+
+    return res.status(200).json({
+      ok: true,
+      msg: 'event created',
+      event: addEvent,
+    });
+  } catch (error) {
+    debug(error.message);
+
+    return res.status(500).json({
+      ok: false,
+      msg: 'internal server error',
+    });
+  }
 };
 
 const updateEvent = (req = request, res = response, next) => {
