7e7018dcae5bfaf1c8bd7c799e7ce20d3ff74b86
carlos alberto angel angel <c.a.a.a.503@gmail.com>
2022-12-05 22:15:51 -0600

    docs: documentar los endpoints de eventos

--- models/Event.model.js
+++ models/Event.model.js
@@ -1,5 +1,60 @@
 const { Schema, model } = require('mongoose');
 
+/**
+ * @openapi
+ * components:
+ *   schemas:
+ *     eventDto:
+ *       type: object
+ *       properties:
+ *         id:
+ *           type: string
+ *         title:
+ *           type: string
+ *         notes:
+ *           type: string
+ *         start:
+ *           type: string
+ *           example: 2022-12-06T04:03:39.147Z
+ *         end:
+ *           type: string
+ *           example: 2022-12-06T04:03:39.147Z
+ *         user:
+ *           type: object
+ *           properties:
+ *             id:
+ *               type: string
+ *             name:
+ *               type: string
+ *     createEventDto:
+ *       type: object
+ *       properties:
+ *         title:
+ *           type: string
+ *         notes:
+ *           type: string
+ *           required: false
+ *         start:
+ *           type: string
+ *           example: 2022-12-06T04:03:39.147Z
+ *         end:
+ *           type: string
+ *           example: 2022-12-06T04:03:39.147Z
+ *     updateEventDto:
+ *       type: object
+ *       properties:
+ *         title:
+ *           type: string
+ *         notes:
+ *           type: string
+ *           required: false
+ *         start:
+ *           type: string
+ *           example: 2022-12-06T04:03:39.147Z
+ *         end:
+ *           type: string
+ *           example: 2022-12-06T04:03:39.147Z
+ */
 const EventSchema = new Schema({
   title: {
     type: String,
--- routes/events.js
+++ routes/events.js
@@ -9,8 +9,101 @@ const { jwtValidator } = require('../middlewares/jwt-validator');
 /** rutas: /api/event */
 
 router.use(jwtValidator);
+
+/**
+ * @openapi
+ * /api/events:
+ *   get:
+ *     tags:
+ *       - events
+ *     summary: obtener los eventos de un usuario
+ *     responses:
+ *       200:
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 ok:
+ *                   type: boolean
+ *                   example: true
+ *                 msg:
+ *                   type: string
+ *                   example: events listed
+ *                 events:
+ *                   type: array
+ *                   items:
+ *                     $ref: '#/components/schemas/eventDto'
+ */
+
 router.get('/', eventController.getEvents);
+
+/**
+ * @openapi
+ * /api/events/{id}:
+ *   get:
+ *     tags:
+ *       - events
+ *     summary: obtener un evento
+ *     parameters:
+ *        - name: id
+ *          in: path
+ *          description: ID del evento
+ *          required: true
+ *          schema:
+ *            type: string
+ *     responses:
+ *       200:
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 ok:
+ *                   type: boolean
+ *                   example: true
+ *                 msg:
+ *                   type: string
+ *                   example: event found
+ *                 event:
+ *                   type: object
+ *                   $ref: '#/components/schemas/eventDto'
+ */
 router.get('/:id', eventController.getEvent);
+
+/**
+ * @openapi
+ * /api/events:
+ *   post:
+ *     tags:
+ *       - events
+ *     summary: crear un evento
+ *     requestBody:
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/createEventDto'
+ *         required: true
+ *     responses:
+ *       201:
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 ok:
+ *                   type: boolean
+ *                   example: true
+ *                 msg:
+ *                   type: string
+ *                   example: event created
+ *                 event:
+ *                   type: object
+ *                   $ref: '#/components/schemas/eventDto'
+ */
 router.post(
   '/',
   [
@@ -21,7 +114,76 @@ router.post(
   ],
   eventController.addEvent,
 );
+
+/**
+ * @openapi
+ * /api/events/{id}:
+ *   put:
+ *     tags:
+ *       - events
+ *     summary: actualizar un evento
+ *     parameters:
+ *        - name: id
+ *          in: path
+ *          description: ID del evento
+ *          required: true
+ *          schema:
+ *            type: string
+ *     requestBody:
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/updateEventDto'
+ *         required: true
+ *     responses:
+ *       200:
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 ok:
+ *                   type: boolean
+ *                   example: true
+ *                 msg:
+ *                   type: string
+ *                   example: event updated
+ *                 event:
+ *                   type: object
+ *                   $ref: '#/components/schemas/eventDto'
+ */
 router.put('/:id', eventController.updateEvent);
+
+/**
+ * @openapi
+ * /api/events/{id}:
+ *   delete:
+ *     tags:
+ *       - events
+ *     summary: eliminar un evento
+ *     parameters:
+ *        - name: id
+ *          in: path
+ *          description: ID del evento
+ *          required: true
+ *          schema:
+ *            type: string
+ *     responses:
+ *       200:
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 ok:
+ *                   type: boolean
+ *                   example: true
+ *                 msg:
+ *                   type: string
+ *                   example: event removed
+ */
 router.delete('/:id', eventController.deleteEvent);
 
 module.exports = router;
